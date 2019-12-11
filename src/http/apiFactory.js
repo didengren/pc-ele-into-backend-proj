79c944de9897e59a55e31a039545444ee048f05b
nianzu.xu@trinasolar.com <nianzu.xu@trinasolar.com>
2019-12-11 10:09:30 +0800

    #

---
+++ src/http/apiFactory.js
@@ -0,0 +1,12 @@
+// import qs from "qs";
+// import { fetch } from "./index";
+
+/**
+ * todo 封装某一个具体的接口请求
+ */
+// export const demoAPI = (field) =>
+//   fetch({
+//     url: "/financial/v1/loginByTfxToken",
+//     method: "get",
+//     parameter: qs.stringify()
+//   });
